bcccb2fa77bfb16f87f6feb35359d0a1c1d1577d
phoenix104104 <phoenix104104@gmail.com>
2018-08-01 00:48:01 -0700

    update download_models

--- pretrained_models/download_models.sh
+++ pretrained_models/download_models.sh
@@ -2,6 +2,6 @@
 
 URL=http://vllab.ucmerced.edu/wlai24/video_consistency/models
 
-wget -N $URL/FlowNet2_checkpoint.pth.tar
 wget -N $URL/ECCV18_blind_consistency.pth
 wget -N $URL/ECCV18_blind_consistency_opts.pth
+wget -N $URL/FlowNet2_checkpoint.pth.tar
